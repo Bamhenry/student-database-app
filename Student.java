ea6367264da768f034d6e7ea75e86e191d939890
Brooke McHenry <mchenry.bam@gmail.com>
2020-12-19 20:51:28 -0600

    Created unique student ID

--- Student.java
+++ Student.java
@@ -1,17 +1,40 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 	private String firstName;
 	private String lastName;
-	private String year;
-	private int studentID;
+	private String gradeYear;
+	private String studentID;
 	private String courses;
 	private int tuitionBalance;
-	private int courseCost = 600;
+	private static int courseCost = 600;
+	private static int id = 1000;
 	
 	//Constructor: prompts user to enter student's name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name: ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senoir\nEnter student class level: ");
+		this.gradeYear = in.nextLine();
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+	}
 	
 	//Generate an ID
+	private void setStudentID() {
+		//Grade level + ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
 	
 	//Enroll in courses
 	
