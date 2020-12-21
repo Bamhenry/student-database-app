a06d48ba833a56d2485b919f61241856876a381e
Brooke McHenry <mchenry.bam@gmail.com>
2020-12-20 19:23:14 -0600

    Created array of students

--- Student.java
+++ Student.java
@@ -7,7 +7,7 @@ public class Student {
 	private String lastName;
 	private String gradeYear;
 	private String studentID;
-	private String courses = null;
+	private String courses = " ";
 	private int tuitionBalance = 0;
 	private static int courseCost = 600;
 	private static int id = 1000;
@@ -25,8 +25,6 @@ public class Student {
 		this.gradeYear = in.nextLine();
 		
 		setStudentID();
-		
-		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 	}
 	
 	//Generate an ID
@@ -40,26 +38,40 @@ public class Student {
 	public void enroll() {
 		//Get inside a loop, user hits 0
 		do {
-			System.out.println("Enter course to enroll (0 to quit): ");
+			System.out.println("Enter course to enroll (Q to quit): ");
 			Scanner in = new Scanner(System.in);
 			String course = in.nextLine();
 			if (!course.equals("Q")) {
-				courses = courses + "\n" + course;
+				courses = courses + "\n  " + course;
 				tuitionBalance += courseCost;
 			}
 			else { break; }
 		} while (1 != 0);
-		
-		System.out.println("ENROLL IN: " + courses);
-		System.out.println("TUITION BALANCE: " + tuitionBalance);	
 	}
-		
-	
 	
 	//View balance
+	public void viewBalance() {
+		System.out.println("Your balance is: $" + tuitionBalance);
+	}
 	
 	//Pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter your payment: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance -= payment;
+		System.out.println("Thank you for your payment of $" + payment);
+		viewBalance();
+	}
 	
 	//Show status
+	public String toString() {
+		return "Name: " + firstName + " " + lastName +
+				"\nGrade Year: " + gradeYear +
+				"\nStudent ID: " + studentID +
+				"\nCourses Enrolled: " + courses +
+				"\nBalance: $" + tuitionBalance;
+	}
 
 }
